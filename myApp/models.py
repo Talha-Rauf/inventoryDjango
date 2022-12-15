87be07f4187318cad882b773b2681ff7b35d4426
Talha-Rauf <bsee16024@itu.edu.pk>
2022-12-15 13:03:02 +0500

    models

--- myApp/models.py
+++ myApp/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
-    user_name = models.CharField(max_length=264, unique=False)
+class Inventory(models.Model):
+    inv_name = models.CharField(max_length=264, unique=False)
 
     def __str__(self):
-        return self.user_name
+        return self.inv_name
 
 
 class UserPage(models.Model):
-    user = models.ForeignKey(User)
-    name = models.CharField(max_length=264, unique=False)
+    user = models.ForeignKey(Inventory, on_delete=models.CASCADE)
+    username = models.CharField(max_length=264, unique=False)
     url = models.URLField(unique=True)
 
     def __str__(self):
@@ -19,7 +19,7 @@ class UserPage(models.Model):
 
 
 class AccessRecord(models.Model):
-    name = models.ForeignKey(UserPage)
+    name = models.ForeignKey(UserPage, on_delete=models.CASCADE)
     date = models.DateField()
 
     def __str__(self):

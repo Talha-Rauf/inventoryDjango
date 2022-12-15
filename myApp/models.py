6fefa5bb5aadeae8c72dee70771010849e0cde2a
Talha-Rauf <bsee16024@itu.edu.pk>
2022-12-15 12:47:54 +0500

    constructing database

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=264, unique=False)
+
+    def __str__(self):
+        return self.user_name
+
+
+class UserPage(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=264, unique=False)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(UserPage)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)

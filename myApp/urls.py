17e4cdeb7727d39eea0606839c07a7737815d05b
Talha-Rauf <bsee16024@itu.edu.pk>
2022-12-21 17:12:45 +0500

    configured delete function

--- myApp/urls.py
+++ myApp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('users/<int:pk>', views.UserDetailedView.as_view(), name='user-detail'),
     # path('users/editUser/<int:pk>', views.EditUserDetail.as_view(), name='edit-user-detail'),
     path('add_user/', views.AddNewUserView.as_view(), name='add-user'),
+    path('<pk>/delete/', views.DeleteUserView.as_view(), name='delete-user'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,17 +1,15 @@
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.views.generic import DetailView
 from . import forms, models
 
 
 # Create your views here.
 class HomePageView(TemplateView):
-
     template_name = "index.html"
 
 
 class UsersInventoryView(TemplateView):
-
     template_name = "users.html"
 
     def get_context_data(self, **kwargs):
@@ -21,16 +19,19 @@ class UsersInventoryView(TemplateView):
 
 
 class UserDetailedView(DetailView):
-
     model = models.User
     template_name = 'per_user.html'
     context_object_name = 'user'
 
 
 class AddNewUserView(CreateView):
-
     model = models.User
     form_class = forms.NewUser
     template_name = 'addUser.html'
     success_url = '/users/'
 
+
+class DeleteUserView(DeleteView):
+    model = models.User
+    success_url = '/users/'
+    template_name = 'confirm_delete.html'
